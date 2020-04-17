37ee43de698715a0fe8413a196c2f4340e3d1558
xiuweili1999 <60372954+xiuweili1999@users.noreply.github.com>
2020-04-17 12:42:00 -0400

    lab_9

--- lab_9/server.js
+++ lab_9/server.js
@@ -1,11 +1,10 @@
 // These are our required libraries to make the server work.
 // We're including a server-side version of Fetch to build on your client-side work
-const express = require('express');
-const fetch = require('node-fetch');
+const express = require("express");
+const fetch = require("node-fetch");
 
 // Here we instantiate the server we're going to turn on
 const app = express();
-
 
 // Servers are often subject to the whims of their environment.
 // Here, if our server has a PORT defined in its environment, it will use that.
@@ -17,31 +16,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // And the ability to serve some files publicly, like our HTML.
-app.use(express.static('public'));
-
-
+app.use(express.static("public"));
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
+  // Enter the URL for the data you would like to retrieve here
+  const baseURL =
+    "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
   // it instead handles returning data to your front end at line 34.
-    fetch(baseURL)
-      .then((r) => r.json())
-      .then((data) => {
-        console.log(data);
-        res.send({ data: data }); // here's where we return data to the front end
-      })
-      .catch((err) => {
-        console.log(err);
-        res.redirect('/error');
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+      const newData = data.reduce((res, cur) => {
+        if (!res[cur.category]) {
+          res[cur.category] = [];
+        }
+        res[cur.category]++;
+        return res;
+      }, {});
+
+      const dataPoints = [];
+
+      Object.keys(newData).forEach((key) => {
+        dataPoints.push({ label: key, y: newData[key] });
       });
+
+      console.log(dataPoints);
+      res.send({ dataPoints }); // here's where we return data to the front end
+    })
+
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/error");
+    });
 }
 
 // This is our first route on our server.
 // To access it, we can use a "GET" request on the front end
 // by typing in: localhost:3000/api or 127.0.0.1:3000/api
-app.get('/api', (req, res) => {processDataForFrontEnd(req, res)});
+app.get("/api", (req, res) => {
+  processDataForFrontEnd(req, res);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
